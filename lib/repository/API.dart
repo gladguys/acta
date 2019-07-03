3b9b8b0ca62c8ee010e9e509dbb5a4ec66e95db4
Rodrigo Bastos <rodrigobastosv@gmail.com>
2019-07-03 08:33:18 -0300

    Organizing this file

--- lib/repository/API.dart
+++ lib/repository/API.dart
@@ -1,2 +1,9 @@
-const String API_KEY = 'f1c8c404c4a140afa9ee7e5ef152b4f6';
-const String API_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=$API_KEY';
+const String API_KEY_VALUE = 'f1c8c404c4a140afa9ee7e5ef152b4f6';
+const String API_KEY = 'apiKey=$API_KEY_VALUE';
+const String API_BASE_URL = 'https://newsapi.org/v2/';
+
+// Endpoints
+const String TOP_HEADLINES = 'top-headlines';
+
+// Query Params
+const String COUNTRY = 'country';
