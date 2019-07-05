bbc7d92008d4890449da06aa847af7d4f0d35354
Rodrigo <rodrigobastosv@gmail.com>
2019-07-05 02:28:16 -0300

    Inicialing last endpoint

--- lib/repository/API.dart
+++ lib/repository/API.dart
@@ -3,8 +3,10 @@ const String API_BASE_URL = 'https://newsapi.org/v2/';
 // Endpoints
 const String TOP_HEADLINES = 'top-headlines';
 const String ALL_SOURCES = 'sources';
+const String EVERYTHING = 'everything';
 
 // Query Params
 const String COUNTRY = 'country';
 const String SOURCES = 'sources';
 const String CATEGORY = 'category';
+const String Q = 'q';
