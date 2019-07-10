6c9e12d1c35583fd25f722efc14323b1722f79bd
Rodrigo <rodrigobastosv@gmail.com>
2019-07-09 23:56:21 -0300

    Using great package

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:bloc_pattern/bloc_pattern.dart';
 import 'package:flutter/material.dart';
 import 'package:global_configuration/global_configuration.dart';
+import 'package:catcher/catcher_plugin.dart';
 
 import './screens/home-screen.dart';
 import 'blocs/news_bloc.dart';
@@ -11,7 +12,20 @@ void main() {
 
 Future<void> _loadApp() async {
   await _loadConfig();
-  runApp(MyApp());
+
+  CatcherOptions debugOptions = CatcherOptions(DialogReportMode(), [
+    EmailAutoHandler(
+        "smtp.gmail.com", 587, "actaflutter@gmail.com", "AcTa Catcher",
+        "acta1234", ["rodrigobastosv@gmail.com"])
+  ]);
+
+  CatcherOptions releaseOptions = CatcherOptions(DialogReportMode(), [
+    EmailManualHandler(["recipient@email.com"])
+  ]);
+
+  Catcher(MyApp(),
+      debugConfig: debugOptions,
+      releaseConfig: releaseOptions);
 }
 
 Future<void> _loadConfig() async {
@@ -25,6 +39,7 @@ class MyApp extends StatelessWidget {
     return BlocProvider(
       child: MaterialApp(
         title: 'Acta',
+        navigatorKey: Catcher.navigatorKey,
         theme: ThemeData(
           primaryColor: Colors.brown[100],
           accentColor: Colors.brown[800],
