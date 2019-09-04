bc5591a29bb48b14e4e50002fd21486a3730dfc2
Alexandre <alexlima.crvg@gmail.com>
2019-09-03 22:23:52 -0300

    Creating first page

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -7,6 +7,47 @@ import 'package:flutter/material.dart';
 import 'package:intro_views_flutter/Models/page_view_model.dart';
 import 'package:intro_views_flutter/intro_views_flutter.dart';
 
+final page0 = PageViewModel(
+  pageColor: Colors.brown[100],
+  bubbleBackgroundColor: Colors.brown[700],
+  textStyle: TextStyle(color: Colors.brown[900]),
+  iconColor: null,
+  title: Container(),
+  mainImage: Column(
+    children: <Widget>[
+      Text(
+        ATLabels.ACTA,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontFamily: 'Italianno',
+          fontSize: 64.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.brown[900],
+        ),
+      ),
+      Image.asset(
+        'assets/images/news.png',
+        height: 250.0,
+        width: 270.0,
+        fit: BoxFit.contain,
+        alignment: Alignment.center,
+      ),
+    ],
+  ),
+  body: Column(
+    children: <Widget>[
+      Text(
+        'As mais recentes notícias do mundo em um só app. Nossa fonte de notícias é o Google.',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.brown[900],
+        ),
+      ),
+    ],
+  ),
+);
+
+
 final page = PageViewModel(
   pageColor: Colors.brown[100],
   bubbleBackgroundColor: Colors.brown[700],
@@ -168,7 +209,7 @@ class IntroScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.brown[800],
       body: IntroViewsFlutter(
-        [page, page2, page3],
+        [page0, page, page2, page3],
         onTapDoneButton: () => _goToHomeScreen(context),
         showSkipButton: true,
         pageButtonTextStyles: TextStyle(
